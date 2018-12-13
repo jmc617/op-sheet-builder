9f8107f8bba4fd7bd21ef08bb86e504dd37ac240
jmc617 <jmc617@gmail.com>
2018-12-12 19:21:05 -0500

    toggle changes opsheet list class

--- src/components/Player/Opsheet/Opsheet.js
+++ src/components/Player/Opsheet/Opsheet.js
@@ -22,28 +22,51 @@ import ListType from './ListType';
 class Opsheet extends Component {
   state = { 
     collapsed: true, 
+    listClassName: 'list-wrapper-collapsed',
   };
+
+  checkIfCollapsed = () => {
+    if (this.state.collapsed === true) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  listClassNameChange = () => {
+    if (this.checkIfCollapsed() === true) {
+      this.setState({listClassName: 'list-wrapper'})
+    } else {
+      this.setState({listClassName: 'list-wrapper-collapsed'})
+    }
+}
 
   toggleSheetCollapseExpand = () => {
     this.setState({collapsed: !this.state.collapsed})
+    this.listClassNameChange();
 
     console.log('Current state='+ this.state.collapsed);
+    console.log(this.state.listClassName);
+
 
   };
-    
-    render() {
+   
+    render(){
+
       return (
         <div className="opsheet-wrapper">
           <h3>Op sheet</h3>
           <button className='collapse-toggle' onClick={this.toggleSheetCollapseExpand}>+/-</button>
           
           <ListType />
-          <List /> 
+          <List listClassName = {this.listClassName}/> 
+
         </div>
       );
     }
   }
 
 export default Opsheet;
+
 
 
