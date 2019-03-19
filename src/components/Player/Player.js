4bbbb0ada18ef93b4a7e793026c93442a30efe89
jmc617 <jmc617@gmail.com>
2019-03-18 21:01:13 -0400

    removed old state from player.js

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -6,30 +6,13 @@ import Search from './Search/Search';
 
 
 class Player extends Component {
-  state = { 
-    opSheetItems: [], 
-  };
-
-  addItemToOpsheet = (name) => {
-    return () => {
-      this.setState(state => {
-        const opSheetItems = state.opSheetItems.concat(name)
-        console.log(opSheetItems);
-        return opSheetItems
-      });
-    }
-  }
 
   render() {
 
-    const { opSheetItems } = this.state;
-
-    
     return (
       <div className="wrapper">
           <div className='nav'>
               <Link to='/'> Home </Link>
-              <div onClick={this.addItemToOpsheet('test')}>add</div>
           </div>
           <Search />
           <Information />
